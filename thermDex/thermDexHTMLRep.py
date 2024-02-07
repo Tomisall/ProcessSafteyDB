5bf5fd18667e6b75d9cb88748bcbec54d8b2a334
Thomas Sheridan <thomas.sheridan@evotec.com>
2024-02-07 17:02:13 +0000

    Finally A Report template I'm mostly happy with!! Just a few easy (fingers crossed) changes left to make :)

--- thermDex/thermDexHTMLRep.py
+++ thermDex/thermDexHTMLRep.py
@@ -32,41 +32,41 @@ def mdReportCreation(molecule, dataURL):
 <h1>Thermal Hazard Assessment Memo</h1>
 </div>
 <h2>{molecule.name}</h2>
-<div style="text-align: center;">
-<img src="{dataURL}" alt="HTML image test" width="50%"/>
+<div class="imgDiv">
+<img src="{dataURL}" alt="HTML image test" style="object-fit: cover;"/>
 </div>              
 <h3>Molecule Properties</h3>
-<p>SMILES: {molecule.SMILES}</p>
-<p>Formula: {molecule.eleComp}</p>
-<p>{mpString}</p>
+<p>SMILES: {molecule.SMILES}<br>
+Formula: {molecule.eleComp}<br>
+MW: {"{:.2f}".format(molecule.MW)} g mol<sup>-1</sup><br>
+{mpString}</p>
 <h3>Results</h3>
-
-<table align="Center" style="border-spacing: 10px; width: 100%;">
-<tr class="even">
-<td colspan="3">High Energy Groups: ({molecule.HEG}) {molecule.HEG_list} &nbsp;</td>
+<table align="Center" class="secretTable">
+<tr class="secretTable">
+<td colspan="3" class="secretTable">High Energy Groups: ({molecule.HEG}) {', '.join(molecule.HEG_list)} &nbsp;</td>
 </tr>
-<tr>
-<td colspan="3">Explosive Groups: ({molecule.EFG}) {molecule.EFG_list}</td>
+<tr class="secretTable">
+<td colspan="3" class="secretTable">Explosive Groups: ({molecule.EFG}) {', '.join(molecule.EFG_list)}</td>
 </tr>
-<tr class="even">
-<td>Rule of Six = {molecule.RoS_val}</td>
-<td>Oxygen Balance = {molecule.OB_val}</td>
-<td> </td>
+<tr class="secretTable" class="secretTable">
+<td class="secretTable">Rule of Six = {molecule.RoS_val}</td>
+<td class="secretTableMid">Oxygen Balance = {"{:.2f}".format(molecule.OB_val)}</td>
+<td class="secretTable"> </td>
 </tr>
-<tr class="even">
-<td>Q<sub>DSC</sub> = {molecule.Q_dsc} {molecule.Qunits}</td>
-<td>T<sub>onset</sub> = {molecule.onsetT}</td>
-<td>T<sub>init</sub> = {molecule.initT}</td>
+<tr class="secretTable">
+<td class="secretTable">Q<sub>DSC</sub> = {molecule.Q_dsc} {molecule.Qunits[:-2]}<sup>-1</sup></td>
+<td class="secretTableMid">T<sub>onset</sub> = {molecule.onsetT} °C</td>
+<td class="secretTable">T<sub>init</sub> = {molecule.initT} °C</td>
 </tr>
-<tr class="even">
-<td>Impact Sensitivity = {molecule.IS_val}</td>
-<td>Explosive Propagation = {molecule.EP_val}</td>
-<td>T<sub>D24</sub> = {molecule.Td24} °C</td>
+<tr class="secretTable">
+<td class="secretTable">Impact Sensitivity = {"{:.2f}".format(molecule.IS_val)}</td>
+<td class="secretTableMid">Explosive Propagation = {"{:.2f}".format(molecule.EP_val)}</td>
+<td class="secretTable">T<sub>D24</sub> = {molecule.Td24} °C</td>
 </tr>
 </table>
 
 <p>O.R.E.O.S. assessment of risk by scale:</p>
-<table>
+<table align="Center" width="65%" >
 <thead>
 <tr class="header">
 <th> <5 g</th>
@@ -88,6 +88,10 @@ def mdReportCreation(molecule, dataURL):
 <p>These results have been calculated using X<sup>1</sup> and they show Y<sup>2</sup>.</p>
 <p><small>[1]: <i>Org. Proc. Res. Dev.,</i> 2011, 2341-2356</small><br>
 <small>[2]: <i>Org. Proc. Res. Dev.,</i> 2011, 2117-2119</small></p>
+<div id="footer_content" style="text-align: center;">
+<p><small>This memo may contain confidential information.</small><br>
+<small><a href="https://github.com/Tomisall/ProcessSafteyDB">ThermalDex</a></small></p>
+</div>
 </body>
 '''
     convert_html_to_pdf(html_content, "./AssessmentMemos/htmlMemo.pdf")
